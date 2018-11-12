1f47b18edadcba2224314422def5400b59089aa6
David Dean <daviddean@Davids-MacBook-Pro-2.local>
2018-11-12 16:47:50 -0500

    mongoURL

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ let bodyParser=require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 let multer = multer({dest: 'uploads/'});
 app.use(bodyParse.raw({type: 'application/json'}));
-const url = /*  here url  */ ;
+const url = "mongodb://admin:password1@ds159993.mlab.com:59993/meals-db" ;
 let sha256 = require("sha256")
